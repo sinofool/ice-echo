95f8d93b858af0c5f36a4f8ff246f3f8b96a6259
baibochun <baibochun@0cebed6c-7021-11de-9895-81829a6523ab>
2009-07-23 06:21:43 +0000

    create 4 more adapter in server side

--- server/cpp/main.cpp
+++ server/cpp/main.cpp
@@ -7,19 +7,35 @@ public:
 
 virtual bool start(int, char*[]) {
 	_adapter = communicator()->createObjectAdapter("EchoSvc");
+	_adapter1 = communicator()->createObjectAdapter("EchoSvc1");
+	_adapter2 = communicator()->createObjectAdapter("EchoSvc2");
+	_adapter3 = communicator()->createObjectAdapter("EchoSvc3");
+	_adapter4 = communicator()->createObjectAdapter("EchoSvc4");
 
 	_manager = new ::echo::EchoManagerI(_deter);
 	_printer = new ::echo::DeterPrinter(_deter);
 	_printer->start().detach();
-	
+
 	_adapter->add(_manager, communicator()->stringToIdentity("M"));
-	
+	_adapter1->add(_manager, communicator()->stringToIdentity("M"));
+	_adapter2->add(_manager, communicator()->stringToIdentity("M"));
+	_adapter3->add(_manager, communicator()->stringToIdentity("M"));
+	_adapter4->add(_manager, communicator()->stringToIdentity("M"));
+
 	_adapter->activate();
-	return true;	
+	_adapter1->activate();
+	_adapter2->activate();
+	_adapter3->activate();
+	_adapter4->activate();
+	return true;
 };
 
 private:
 	Ice::ObjectAdapterPtr _adapter;
+	Ice::ObjectAdapterPtr _adapter1;
+	Ice::ObjectAdapterPtr _adapter2;
+	Ice::ObjectAdapterPtr _adapter3;
+	Ice::ObjectAdapterPtr _adapter4;
 	::echo::Deter _deter;
 	::echo::EchoManagerI* _manager;
 	::echo::DeterPrinter* _printer;
